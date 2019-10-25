7abcfe1fb9af8189cdf37d13bbfaff83d3b10dbf
leechikit <leechikit0823@163.com>
2019-10-25 16:37:46 +0800

    feat: 表单拖拽

---
+++ client/src/components/controls/controlSettingMap.js
@@ -0,0 +1,13 @@
+export default {
+  FormTextBox: {
+    text: '单行文本',
+    designProperties: [
+      {
+        name: 'displayName',
+        text: '控件名称',
+        defaultValue: '单行文本',
+        description: ''
+      }
+    ]
+  }
+}
